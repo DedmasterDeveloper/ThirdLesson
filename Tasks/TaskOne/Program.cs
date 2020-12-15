435f989519b76a61eb19066e9990e508eff69772
Dedmaster <dedmaster.developer@yandex.ru>
2020-12-15 14:01:04 +0300

    Выполненые первое и второе здания

---
+++ Tasks/TaskOne/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskOne
+{
+    /*
+     * Задание:
+     * а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры;
+     * б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса;
+     */
+    class Complex
+    {
+        /// <summary>
+        /// Действительная часть комплексного числа
+        /// </summary>
+        //private double _re;
+
+        /// <summary>
+        /// Мнимая часть комплексного числа
+        /// </summary>
+        private double _im;
+
+        /// <summary>
+        /// Свойство Re для поля re
+        /// </summary>
+        public double Re { get; set; }
+
+        public double Im
+        {
+            get { return _im; }
+            set
+            {
+                if (value > 0)
+                    _im = value;
+                else
+                    throw new Exception("Мнимая часть комплексного числа должна быть > 0");
+            }
+        }
+
+        public Complex(double re, double im)
+        {
+            Re = re;
+
+            if (im > 0)
+                this._im = im;
+            else
+                throw new Exception("Мнимая часть комплексного числа должна быть > 0");
+        }
+
+        public Complex()
+        {
+
+        }
+
+        public Complex Plus(Complex x)
+        {
+            Complex y = new Complex();
+            y.Re = Re + x.Re;
+            y._im = _im + x._im;
+            return y;
+        }
+
+        public Complex Minus (Complex x)
+        {
+            Complex y = new Complex();
+            y.Re = Re - x.Re;
+            y._im = _im - x._im;
+            return y;
+        }
+
+        public Complex Multiplication(Complex x)
+        {
+            Complex y = new Complex();
+            y.Re = Re * x.Re;
+            y._im = _im * x._im;
+            return y;
+        }
+
+        public override string ToString()
+        {
+            return $"{Re} + {_im}i";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex complexFirst = new Complex();
+            complexFirst.Re = 10;
+            complexFirst.Im = 2;
+            Complex complexSecond = new Complex(23, 5);
+            Console.WriteLine($"Комплексные числа: {complexFirst} и {complexSecond}");
+            Console.WriteLine($"Сложение комплексных чисел: {complexFirst.Plus(complexSecond)}");
+            Console.WriteLine($"Вычитание комплексных чисел: {complexFirst.Minus(complexSecond)}");
+            Console.WriteLine($"Произведение комплексных чисел: {complexFirst.Multiplication(complexSecond)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tasks/TaskTwo/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTwo
+{
+    /*
+     * Задание:
+     * а) С клавиатуры вводятся числа, пока не будет введен 0 (каждое число в новой строке). Требуется подсчитать сумму всех нечетных положительных чисел. Сами числа и сумму вывести на экран, используя tryParse;
+     * б) Добавить обработку исключительных ситуаций на то, что могут быть введены некорректные данные. При возникновении ошибки вывести сообщение. Напишите соответствующую функцию;
+     */
+    class ActionsWithNumbers
+    {
+        private static int _sum;
+        private int _inputNumber;
+        public int inputNumber
+        {
+            get { return _inputNumber; }
+            set
+            {
+                _inputNumber = value;
+                if (_inputNumber % 2 != 0 && _inputNumber > 0)
+                {
+                    _sum += _inputNumber;
+                }
+            }
+        }
+        public ActionsWithNumbers()
+        {
+
+        }
+        public ActionsWithNumbers(int number)
+        {
+            _inputNumber = number;
+            if(_inputNumber % 2 != 0 && _inputNumber > 0)
+            {
+                _sum += _inputNumber;
+            }
+        }
+
+        public int Sum()
+        {
+            return _sum;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводите с клавиатуры числа, для завершения введите (0).");
+            ActionsWithNumbers actionsWithNumbers = new ActionsWithNumbers();
+            while (true)
+            {
+                bool checkingTheNumber = int.TryParse(Console.ReadLine(), out int number);
+                if(checkingTheNumber)
+                {
+                    if (number == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        actionsWithNumbers.inputNumber = number;
+                    }
+                }
+                else
+                    Console.WriteLine("Вы ввели не коректное число.");
+                
+            }
+
+            Console.WriteLine($"Сумма нечетных положительных чисел: {actionsWithNumbers.Sum()} ");
+            Console.WriteLine("Для завершения нажмите Enter");
+            Console.ReadLine();
+        }
+    }
+}
